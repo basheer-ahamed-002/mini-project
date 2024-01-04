d00f26e9cfed573587d58320a35800677c9fbba1
basheer-ahamed-002 <143486917+basheer-ahamed-002@users.noreply.github.com>
2024-01-04 03:48:08 +0000

    hai

--- src/index2.js
+++ src/index2.js
@@ -1,9 +1,12 @@
 const email=document.querySelector(".email");
 const password=document.querySelector(".password");
-const name=document.querySelector(".name")
+const name1=document.querySelector(".name")
 
 function btn(){
-    if(email.value ===''||password.value ===''||name.value===''){
+    if(email.value ===''||password.value ===''||name1.value===''){
+        alert("pls fill the form completely")
+    }
+    else if(email.value===''||password.value===''){
         alert("pls fill the form completely")
     }
     else{
